bd5a7d8cf5f5428d0046b369648e3d5ff219c99e
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-15 14:00:34 -0400

    added checkbox

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,8 +2,15 @@ export default function Todo(props)
 {
     const { todo } = props
     return (
-        <div>
-            <p>{todo.text}</p>
-        </div>
+        <li>
+            <div>
+                <p>{todo.text}</p>
+            </div>
+
+            <label>
+                Complete
+                <input type="checkbox" checked={todo.completed} />
+            </label>
+        </li>
     )
 }
